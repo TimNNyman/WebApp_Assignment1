011d05992e58924afdae2cae320864cd043628f9
TimNNyman <36240398+TimNNyman@users.noreply.github.com>
2018-09-10 13:28:46 +0200

    added timer to script

--- script/index.js
+++ script/index.js
@@ -3,7 +3,19 @@ window.onload = function() {
 }
 
 function setup() {
+    startTimer();
 }
 
-function flipImage() {
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("scrollingImg").src = images[x];
 }
+
+function startTimer() {
+    setInterval(displayNextImage, 3000);
+}
+
+var images = [], x = 0;
+images[0] = "images/scrollImg1.jpg";
+images[1] = "images/scrollImg2.jpg";
+images[2] = "images/scrollImg3.jpg";
